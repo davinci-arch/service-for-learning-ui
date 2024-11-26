df3c3deb7760fa06ba52d877109870057d61e68c
M1ls3 <sofarua@gmail.com>
2024-11-26 13:41:26 +0200

    Fixed Login page

---
+++ src/components/Login/Button.js
@@ -0,0 +1,12 @@
+// src/components/Button.js
+import React from 'react';
+
+const Button = ({ text, onClick }) => {
+  return (
+    <button className="btn" onClick={onClick}>
+      {text}
+    </button>
+  );
+};
+
+export default Button;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import React from 'react';
+import Login from './components/Login/Login'; // Import the Login component
 
-createRoot(document.getElementById('root')).render(
+// Get the root element from the HTML file
+const rootElement = document.getElementById('root');
+
+// Create and render the root
+createRoot(rootElement).render(
   <StrictMode>
-  </StrictMode>,
-)
+    <Login /> {/* Render the Login component */}
+  </StrictMode>
+);
