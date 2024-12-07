5047f3316bc9c02ef9f46d4fd9220344abc51c36
parfesa.a15@gmail.com <parfesa.a15@gmail.com>
2024-12-07 21:15:56 +0200

    [feature]: completed task page for teacher

---
+++ src/components/ObjectPage/TaskObject/ContainerForCompleteTask.jsx
@@ -0,0 +1,15 @@
+
+export default function ContainerForCompleteTask() {
+    return (
+        <aside>
+            <div className="score">Оцінка: 33/100</div>
+            <button className="file-btn">File</button>
+            <button className="link-btn">Link</button>
+            <div className="separator"></div>
+            <button className="add-file-btn">Додати файл</button>
+            <button className="add-link-btn">Посилання</button>
+            <div className="separator"></div>
+            <button className="submit-btn">Здати роботу</button>
+        </aside>
+    )
+}
--- src/components/ObjectPage/TaskObject/TaskObjectPage.jsx
+++ src/components/ObjectPage/TaskObject/TaskObjectPage.jsx
@@ -3,6 +3,7 @@ import NavigationPanel from "../NavigationPanel"
 import "../../styles/TaskObjectPage.scss"
 import CommentForTask from "./CommentForTask"
 import { useState } from "react"
+import ContainerForCompleteTask from "./ContainerForCompleteTask"
 export default function TaskObjectPage() {
 
     const [comments, setComments] = useState([
@@ -11,8 +12,16 @@ export default function TaskObjectPage() {
         { commentAuthorName: "Парфеса Олександр Олексійович", commentDate: "12.09.2024", commentText: "Hdldflsdjfldsfjlsfjdjfjl" },
     ])
 
-    const [deadline, setDeadline] = useState("12.09.2024")
+    const [deadline, setDeadline] = useState("2024-09-12")
 
+    const [roleName, setRoleName] = useState("Teacher")
+    const [taskIsPractical, setTaskIsPractical] = useState(false)
+
+    const formatDate = (date) => {
+        if (!date) return "";
+        const [year, month, day] = date.split("-");
+        return `${day}.${month}.${year}`;
+    };
 
     return (
         <div className="task-obj-page-container">
@@ -20,37 +29,69 @@ export default function TaskObjectPage() {
                 <NavigationPanel />
             </div>
             <div className="task-container">
+                {roleName == "Teacher" ?
+                    <div className="task-container-header">
+                        <span>Тип завдання: </span>
+                        <div className="type-task">
+                            <div className={taskIsPractical ? "type-task-btn active" : "type-task-btn"} onClick={() => setTaskIsPractical(true)}>Практичне</div>
+                            <div className={!taskIsPractical ? "type-task-btn active" : "type-task-btn"} onClick={() => setTaskIsPractical(false)}>Теоретичне</div>
+                        </div>
+                    </div>
+                    : null}
                 <main>
                     <section className="assignment-details">
                         <div className="assignment-header">
                             <span className="assignment-title">Назва завдання</span>
-                            <span className="assignment-deadline">Строк здачі завдання: {deadline}</span>
+                            <div className="assignment-date-container">
+                                <span className="assignment-deadline">Строк здачі завдання: {formatDate(deadline)}</span>
+                                {roleName == "Teacher" ?
+                                    <div>
+                                        <button
+                                            className="type-task-btn"
+                                            onClick={() => document.getElementById("date-input").showPicker()}
+                                        >
+                                            Змінити дату
+                                        </button>
+                                        <input
+                                            type="date"
+                                            id="date-input"
+                                            value={deadline}
+                                            onChange={(e) => setDeadline(e.target.value)}
+                                            style={{ display: "none" }}
+                                        />
+                                    </div>
+                                    : null}
+                            </div>
                         </div>
                         <div className="assignment-description">
                             <h3>Опис завдання</h3>
-                            <div>LSDJFldjfldsjflsdfjlfjsd</div>
+                            {roleName == "Teacher" ?
+                                <div className="create-section">
+                                    <div className="create-section-icon">
+                                        <span className="plus">+</span>
+                                    </div>
+                                    <p className="create-section-text">Додати опис</p>
+                                </div>
+                                : null}
                         </div>
                         <div className="attached-files">
                             <h3>Прикріплені файли</h3>
+                            <div>
+                                <img src="/src/assets/addTask.png" alt="Add file for task" className="add-file-to-task-img" />
+                            </div>
                         </div>
                     </section>
 
-                    <aside>
-                        <div className="score">Оцінка: 33/100</div>
-                        <button className="file-btn">File</button>
-                        <button className="link-btn">Link</button>
-                        <div className="separator"></div>
-                        <button className="add-file-btn">Додати файл</button>
-                        <button className="add-link-btn">Посилання</button>
-                        <div className="separator"></div>
-                        <button className="submit-btn">Здати роботу</button>
-                    </aside>
-                </main>
+                    {taskIsPractical ? <ContainerForCompleteTask /> : null}
 
+                </main>
+                <div className="save-task-settings">
+                    <button id="save-task-settings-btn">Зберегти зміни</button>
+                </div>
                 <section className="comments">
                     <h3>Коментарі до завдання</h3>
-                    {comments.map((value) => (
-                        <CommentForTask commentData={value} />
+                    {comments.map((value, index) => (
+                        <CommentForTask commentData={value} key={index} />
                     ))}
                     <div className="comment-form">
                         <input type="text" placeholder="Коментар" />
